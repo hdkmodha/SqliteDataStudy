b4d14b635534b54a4a9194e2974533e8b77ac31a
Hardik Modha <hardik.modha27@gmail.com>
2026-03-23 19:11:56 +0530

    Initial Commit

---
+++ SqliteDataStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SqliteDataStudy
+//
+//  Created by Hardik Modha on 23/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SqliteDataStudy/SqliteDataStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SqliteDataStudyApp.swift
+//  SqliteDataStudy
+//
+//  Created by Hardik Modha on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SqliteDataStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
